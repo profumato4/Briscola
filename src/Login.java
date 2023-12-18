de657174ccd5cbbcb79ec347c8e7a93f63938d7b
profumato4 <Leonardobelli2006@gmail.com>
2023-12-18 18:12:15 +0100

    Login and register

--- src/Login.java
+++ src/Login.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.swing.JButton;
