04cca8231711ac0285c219d586dfc4ba3061ea80
profumato4 <Leonardobelli2006@gmail.com>
2023-12-22 23:25:08 +0100

    remember login with mac address system + new theme song

--- src/Giocatore.java
+++ src/Giocatore.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 
 public class Giocatore {
 	
-	private String nickName;
+	private final String nickName;
 	private ArrayList<Carta> mano;
 	private int punteggio;
-	
+
 	public Giocatore(String nickName) {
 		this.nickName = nickName;
 		this.mano = new ArrayList<Carta>();
